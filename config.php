efd6a8f583065c98d1ad64107b4b1dd86b69a43a
Jeyaram alaguramanujam <jeyarammca@gmail.com>
2020-09-11 22:22:20 +0530

    Create config.php
    Config file

---
+++ config.php
@@ -0,0 +1,5 @@
+  
+<?php
+	define( 'INSTAGRAM_APP_ID', 'YOUR-APP-ID' );  /*Get it From, FB APP: PRODUCTS => Instagram Basic Display => Instagram App ID*/
+	define( 'INSTAGRAM_APP_SECRET', 'YOUR-APP-SECRET' ); /*Get it From, FB APP: PRODUCTS => Instagram Basic Display => Instagram App Secret*/
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'YOUR-REDIRECT-URI' ); /*Get it From, FB APP: PRODUCTS => Instagram Basic Display => Valid OAuth Redirect URI */
